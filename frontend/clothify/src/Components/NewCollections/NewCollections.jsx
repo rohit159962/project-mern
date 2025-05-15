a7e83f727cf614a44a2657300bdfa2f100eac6c4
rohit159962 <rthakur22270@outlook.com>
2025-05-15 11:35:26 +0530

    Update NewCollections.jsx

--- frontend/clothify/src/Components/NewCollections/NewCollections.jsx
+++ frontend/clothify/src/Components/NewCollections/NewCollections.jsx
@@ -7,7 +7,7 @@ import { useEffect } from "react";
 export const NewCollections = () => {
   const[new_collection,setNew_collection] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:4000/newcollections")
+    fetch("https://project-mern-rdok.onrender.com/newcollections")
       .then((res) => res.json())
       .then((data) => setNew_collection(data));
   },[])
