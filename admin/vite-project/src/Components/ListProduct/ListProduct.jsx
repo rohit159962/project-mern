73311661b85fa2ef78f996810eb1e93142709f99
rohit159962 <rthakur22270@outlook.com>
2025-05-15 11:34:48 +0530

    Update ListProduct.jsx

--- admin/vite-project/src/Components/ListProduct/ListProduct.jsx
+++ admin/vite-project/src/Components/ListProduct/ListProduct.jsx
@@ -4,7 +4,7 @@ import cross_icon from "../../assets/cross_icon.png"
 const ListProduct = () => {
     const [all_products, setAllProducts] = useState([]);
     const fetchInfo = async () => {
-      await fetch("http://localhost:4000/allproducts")
+      await fetch("https://project-mern-rdok.onrender.com/allproducts")
         .then((res) => res.json())
         .then((data) => {
           setAllProducts(data);
@@ -14,7 +14,7 @@ const ListProduct = () => {
       fetchInfo();
     }, []);
     const remove_product = async (id) => {
-      await fetch(`http://localhost:4000/removeproduct`, {
+      await fetch(`https://project-mern-rdok.onrender.com/removeproduct`, {
         method: "POST",
         headers: {
           Accept: "application/json",
